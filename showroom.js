a211c8ce4147e3065d3985cd0ec04f67be005108
burakhrk <81756834+burakhrk@users.noreply.github.com>
2025-03-30 16:41:17 +0300

    Update showroom.js

--- showroom.js
+++ showroom.js
@@ -1,6 +1,6 @@
 // --- Basic Setup ---
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xcccccc); // Room background/fog color
+scene.background = new THREE.Color(0xff0000); // Bright Red
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.y = 1.6; // Simulate average eye height
